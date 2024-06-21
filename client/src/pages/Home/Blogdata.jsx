384946b5d16f73d0d1d1e84d238b7ebadc71ac78
Muskaan <muskan@example.com>
2024-06-21 09:39:02 +0530

    blog page

---
+++ client/src/pages/Home/Blogdata.jsx
@@ -0,0 +1,97 @@
+
+export const blogData = {
+    blogs: [
+      {
+        id: 1,
+        title: "UP Board Results",
+        createdAt: "2024-06-01T00:00:00Z",     
+        imageUrl: "https://dl.dropboxusercontent.com/scl/fi/mwlo6ji0rafo43x7dhuf1/Copy-of-Untitled.png?rlkey=3i8cglr50x3qed5rrpzix41oy&st=yd0yeq1a&dl=0",
+        description: `Filled with a sense of accomplishment and jubilation, we proudly present to you all the class 10th UP board results of our students at Anokhi पहल.
+  Their remarkable performance in the recent examinations is a testament to their unwavering dedication and innate brilliance. We extend our heartfelt congratulations to the students for this outstanding achievement.
+  As they embark on the next chapter of their journey, we wish them a future filled with boundless opportunities, growth and fulfilment.`,
+        link: "#",
+        instagramVideoLink: "https://www.instagram.com/p/C6E3Kmyh7_p/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+      },
+      
+      {
+        id: 2,
+        title: "CBSE Board Results",
+        createdAt: "2024-06-01T00:00:00Z",     
+        imageUrl: "https://dl.dropboxusercontent.com/scl/fi/zor3k5l47hwrj8fky3ttz/Copy-of-Untitled-17.png?rlkey=k89envhqx18xe8ndp5jiakcp8&st=8giuuebs&dl=0",
+        description: `Filled with a sense of accomplishment and jubilation, we proudly present to you all the class 10th CBSE board results of our students at Anokhi पहल.
+  Their remarkable performance in the recent examinations is a testament to their unwavering dedication and innate brilliance. We extend our heartfelt congratulations to the students for this outstanding achievement.
+  As they embark on the next chapter of their journey, we wish them a future filled with boundless opportunities, growth and fulfilment.`,
+        link: "#",
+        instagramVideoLink: "https://www.instagram.com/p/C7WDKLpOKiS/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+      },
+      
+      
+      
+      {
+        id: 3,
+        title: "Holi Celebration",
+        createdAt: "2024-06-02T00:00:00Z",     
+        imageUrl: "https://dl.dropboxusercontent.com/scl/fi/o2ynz7nk7gizdisldvcrk/Copy-of-Untitled-21.png?rlkey=aw0mf45qm0topbf2gu3d8053c&st=xb6qd6qx&dl=0",
+        description: ` Holi, much like its colorful splendor, embodies a deeper essence as a festival of togetherness, joy, and renewal of bonds.
+  In the true spirit of Holi, our volunteers embarked on a journey of vibrant celebration, spreading laughter and happiness in nearby communities. Amidst the playful hues of gulal, we danced, sang, and shared sweets with children and their families, igniting hearts with the warmth of camaraderie.`,
+        link: "#",
+        instagramVideoLink: "https://www.instagram.com/p/C47FsfWR7Th/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+      },
+      {
+        id: 4,
+        title: "Embracing Adolescence",
+        createdAt: "2024-06-03T00:00:00Z",
+        imageUrl: "https://dl.dropboxusercontent.com/scl/fi/yp90lpboy0p1e9frmh2u6/Copy-of-Untitled-23.png?rlkey=v6rd7o8l9qtfn4vq62my002t7&st=dzih2uvo&dl=0",
+        description: ` Embarking on the Journey of Puberty: Anokhi Pehel hosts a transformative session aimed at fostering awareness and understanding of the unique stages of puberty in both girls and boys. With a focus on empowerment and support, this enlightening event equips young minds with the knowledge and tools needed to navigate this significant phase of growth with confidence and resilience, paving the way for a healthier and more informed transition into adolescence and beyond.`,
+        link: "#",
+      },
+      {
+        id: 5,
+        title: "Diwali Celebration",
+        createdAt: "2024-06-04T00:00:00Z",     
+        imageUrl: "https://dl.dropboxusercontent.com/scl/fi/3fzv18n36bgqfawi9qu27/Copy-of-Untitled-18.png?rlkey=ri0z5ihh8rsmloyt18earb8i5&st=yvwj6gjt&dl=0",
+        description: "Diwali, beyond its traditional rituals, embodies a deeper essence as a festival of love, compassion, and serenity.In the true spirit of Diwali, our volunteers visited nearby localities, lit diyas with children and their families, and distributed sweets to them. The laughter and excitement echoed as crackers added a spark of delight to the celebration.",
+        link: "#",
+        instagramVideoLink: "https://www.instagram.com/p/CzlSPp3RnR2/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+      },
+      {
+        id: 6,
+        title: "Antyodaya 2023",
+        createdAt: "2024-06-05T00:00:00Z",     
+        imageUrl: "https://dl.dropboxusercontent.com/scl/fi/lp4v7la6fvjxzle8hztw1/Copy-of-Untitled-19.png?rlkey=12kd35z7t26p0sehriw454bmj&st=2l0oh3au&dl=0", // Use the imported image here
+        description: " A Day of Creativity & Joy! Step into a world of boundless imagination, as we offer a glimpse of our unique Children's Day celebration.On November 8, 2023, get ready for a day filled with learning, laughter, and the brilliance of young talents on full display.",
+        link: "#",
+        instagramVideoLink: "https://www.instagram.com/p/Czni12xxt9n/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+      },
+      {
+        id: 7,
+        title: "Cloth Donation",
+        createdAt: "2024-06-05T00:00:00Z",     
+        imageUrl: "https://dl.dropboxusercontent.com/scl/fi/30inj8d1a2b5o2vyrxieg/Copy-of-Untitled-6.png?rlkey=oup883js0pn4o4uskfo8gf3dq&st=lgscju8z&dl=0", // Use the imported image here
+        description: " A Day of Creativity & Joy! Step into a world of boundless imagination, as we offer a glimpse of our unique Children's Day celebration.On November 8, 2023, get ready for a day filled with learning, laughter, and the brilliance of young talents on full display.",
+        link: "#",
+        instagramVideoLink: "https://www.instagram.com/p/Cy7_eFJRMXs/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+      },
+      {
+        id: 8,
+        title: "Teachers Day Celebration",
+        createdAt: "2024-06-05T00:00:00Z",     
+        imageUrl: "https://dl.dropboxusercontent.com/scl/fi/ency334edm5ujbcuw9hsi/Copy-of-Untitled-24.png?rlkey=cjav5v2j9reugd4dsd471n50k&st=8zkliajd&dl=0", // Use the imported image here
+        description: "  A teacher's touch opens not only hands but also young minds and hearts, cultivating a thirst for knowledge that makes life rich with curiosity.At Anokhi Pehel, we hold our volunteers in the highest regard for the invaluable knowledge they impart and the profound love they shower upon our children, leaving an indelible mark on their hearts. ",
+        link: "#",
+        instagramVideoLink: "https://www.instagram.com/p/Cw11KJrRaVO/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+      },
+     
+      {
+        id: 9,
+        title: "Independence Day'23",
+        createdAt: "2024-06-05T00:00:00Z",     
+        imageUrl: "https://dl.dropboxusercontent.com/scl/fi/7f5e20t82itkk1sth5hof/Copy-of-Untitled-22.png?rlkey=hmpthuk3y48yz1ydzn919o2dh&st=hxzx88nc&dl=0", // Use the imported image here
+        description: " With hearts brimming with pride and joy, we joyfully share the incredible tale of our 77th Independence Day celebration at Anokhi Pehel.The spirit of patriotism danced alongside our students.We curated a diverse array of events that ignited patriotism within our students. ",
+        link: "#",
+        instagramVideoLink: "https://www.instagram.com/p/CwCuqWSxHHf/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+      },
+      
+    ],
+  };
+  

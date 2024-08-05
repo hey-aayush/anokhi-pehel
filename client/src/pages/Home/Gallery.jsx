cf7cfffc84d1e10afea9e918ad80a4c7b0b39fa0
SaurabhChaudhary39 <127103821+SaurabhChaudhary39@users.noreply.github.com>
2024-08-05 14:04:56 +0530

    Update Gallery.jsx

--- client/src/pages/Home/Gallery.jsx
+++ client/src/pages/Home/Gallery.jsx
@@ -94,7 +94,7 @@ const Gallery = () => {
           <div>
             <img
               className="h-auto max-w-full rounded-lg"
-              src="https://res.cloudinary.com/dyuzmmpzb/image/upload/v1719129277/fqogqmvusopeo5qlgt2l.jpg"
+              src="https://res.cloudinary.com/dyuzmmpzb/image/upload/v1722846570/ap_news_znmrdr.jpg"
               alt=""
             />
           </div>

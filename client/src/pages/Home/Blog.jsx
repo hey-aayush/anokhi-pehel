72f098c7cc0cee82f38d9e17e17b2ea79e19fc6e
SaurabhChaudhary39 <127103821+SaurabhChaudhary39@users.noreply.github.com>
2025-04-02 01:40:02 +0530

    Update Blog.jsx

--- client/src/pages/Home/Blog.jsx
+++ client/src/pages/Home/Blog.jsx
@@ -147,11 +147,6 @@ const Blog = () => {
                       />
                     </a>
                     <div className="p-5">
-                      <a href={blog.link}>
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-black dark:text-white">
-                          {blog.title}
-                        </h5>
-                      </a>
                       <p className="mb-3 font-normal text-gray-700 dark:text-gray-300">
                         {getSnippet(blog.description, isExpanded)}
                       </p>
